f2a58c61600eeaf7d1c110d50da98a54349a427b
Rana Zaeem <ranazaeem34@gmail.com>
2025-02-11 14:30:09 +0500

    basic boler

---
+++ src/modules/user.module.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password?: string;
+}
+
+const userSchema: Schema<IUser> = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String,optional: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model<IUser>('User', userSchema);
+export default User;
